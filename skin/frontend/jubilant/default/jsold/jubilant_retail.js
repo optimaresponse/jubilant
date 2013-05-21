190bb60df5d55923f6bafc5e255a85a7749c119e
sivakumar <sivakumar@optimaresponse.com>
2013-05-21 13:45:36 +0530

    Adding jubilant project to git

---
+++ skin/frontend/jubilant/default/jsold/jubilant_retail.js
@@ -0,0 +1,21 @@
+(function($){
+			$(window).load(function(){
+				
+				$(".vscroll_div").mCustomScrollbar({
+					scrollButtons:{
+						enable:true
+					}
+				});
+				
+				$(".hscroll_div").mCustomScrollbar({
+					horizontalScroll:true,
+					scrollButtons:{
+						enable:true
+					}
+				});
+				
+				
+			});
+			
+		})(jQuery);
+		
